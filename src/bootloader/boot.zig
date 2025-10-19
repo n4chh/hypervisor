47dda05ff481402f93b610b79b9344600b02885b
n4chh <iortego42madrid@gmail.com>
2025-10-19 23:40:59 +0200

    hello world before efi

--- src/bootloader/boot.zig
+++ src/bootloader/boot.zig
@@ -2,7 +2,15 @@ const std  = @import("std");
 const uefi = std.os.uefi;
 
 pub fn main() uefi.Status {
+    // var status: uefi.Status = undefined;
+    const con_out = uefi.system_table.con_out orelse return .aborted;
+
+    con_out.clearScreen() catch unreachable;
+    for ("Hello, World\n") |b| {
+        _ = con_out.outputString(&[_:0]u16{b}) catch unreachable;
+    }
     while (true)
         asm volatile ("hlt");
+
     return .success;
 }
