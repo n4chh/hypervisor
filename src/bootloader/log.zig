753d533c8a096be4f1f3a6b8778a9554b695872e
n4chh <igortego42madrid@gmail.com>
2026-06-05 10:02:40 +0200

    minor changes

--- src/bootloader/log.zig
+++ src/bootloader/log.zig
@@ -31,13 +31,13 @@ const LogLevel: std.log.Level = @enumFromInt(@intFromEnum(build_options.log_leve
 fn log(comptime level: std.log.Level, comptime scope: @TypeOf(.enum_literal), comptime fmt: []const u8, args: anytype) void {
     const scope_str = if (scope == .default) ": " else "(" ++ @tagName(scope) ++ "): ";
     const level_str ="[" ++  switch (level)  {
-        .debug => "\x1b[1;32m",
+        .debug => "\x1b[1;36m",
         .info => "\x1b[1;34m",
         .warn => "\x1b[1;33m",
         .err => "\x1b[1;31m",
     } ++ @tagName(level) ++ "\x1b[0m] ";
     // This function has changed from version 15.
-    if (@intFromEnum(LogLevel) > @intFromEnum(level)) return;
+    if (@intFromEnum(LogLevel) < @intFromEnum(level)) return;
     std.Io.Writer.print(&UefiWriter, level_str ++ scope_str ++ fmt ++ "\r\n", args) catch unreachable;
 }
 
